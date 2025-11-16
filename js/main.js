9855f7f34f53579ad5ef6d7d66bae977aa7981f6
ZachariaStrange <zstran5871@gmail.com>
2025-11-16 14:11:54 -0500

    Added the index, style, and loading in the data in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// js/main.js
+d3.csv("data/Student_performance_data.csv", d => ({
+  Age: +d.Age,
+  GPA: +d.GPA,
+  StudyTimeWeekly: +d.StudyTimeWeekly,
+  ParentalEducation: +d.ParentalEducation,
+  ParentalSupport: +d.ParentalSupport
+})).then(data => {
+  drawStudyTimeVsGPA(data);
+  drawAgeVsGPA(data);
+  drawParentalInfluence(data);
+});
 
